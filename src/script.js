b5d5a28f1b58a36a9b1a83b43a369482e6267c6f
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-04-21 22:22:51 -0300

    update script and styles overflow in pre and div

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ const $p = $("#description");
 const $footer = $("footer");
 const fileName = "historia.txt";
 const stylesData = [
-  "background-color: #3163c66e; color: #fff; text-wrap: balance; padding: 10px; border: 1px solid #666; font-family: system-ui; border-radius: 8px;",
+  "background-color: #3163c66e; color: #fff; text-wrap: balance; padding: 10px; border: 1px solid #666; font-family: system-ui; border-radius: 8px; overflow: auto;",
 ];
 $h1.innerHTML = "Clase de Git Bash • UTN-FRSR";
 $h2.innerHTML = "Historia de Git";
@@ -26,48 +26,48 @@ $footer.innerHTML = `${new Date().getFullYear()} &copyGabriel Calcagni`;
 function createList() {
   $pre.innerHTML = `
   <li>Comandos básicos de Git Bash</li>
-  <pre>
-    <span style="color: gray;"># Inicializar un nuevo repositorio Git en un directorio existente</span>
-    git init
+<pre>
+<span style="color: gray;"># Inicializar un nuevo repositorio Git en un directorio existente</span>
+git init
   
-    <span style="color: gray;"># Clonar un repositorio Git existente desde una URL remota</span>
-    git clone "URL"
+<span style="color: gray;"># Clonar un repositorio Git existente desde una URL remota</span>
+git clone "URL"
   
-    <span style="color: gray;"># Agregar archivos al área de preparación para su posterior confirmación</span>
-    git add "file"
+<span style="color: gray;"># Agregar archivos al área de preparación para su posterior confirmación</span>
+git add "file"
   
-    <span style="color: gray;"># Confirmar los cambios en el repositorio con un mensaje descriptivo</span>
-    git commit -m "Mensaje de confirmación"
+<span style="color: gray;"># Confirmar los cambios en el repositorio con un mensaje descriptivo</span>
+git commit -m "Mensaje de confirmación"
   
-    <span style="color: gray;"># Configurar nuestras credenciales en git</span>
-    git config user.email "micorreo-electronico@mail.com"
-    git config user.name "Mi Nombre"
+<span style="color: gray;"># Configurar nuestras credenciales en git</span>
+git config user.email "micorreo-electronico@mail.com"
+git config user.name "Mi Nombre"
   
-    <span style="color: gray;"># Ver el estado actual del repositorio, incluyendo archivos sin seguimiento, modificados y en el área de preparación</span>
-    git status
+<span style="color: gray;"># Ver el estado actual del repositorio, incluyendo archivos sin seguimiento, modificados y en el área de preparación</span>
+git status
   
-    <span style="color: gray;"># Ver un registro de cambios y confirmaciones en el repositorio</span>
-    git log
+<span style="color: gray;"># Ver un registro de cambios y confirmaciones en el repositorio</span>
+git log
   
-    <span style="color: gray;"># Crear una nueva rama para trabajar en paralelo con la rama actual</span>
-    git branch <branch_name>
+<span style="color: gray;"># Crear una nueva rama para trabajar en paralelo con la rama actual</span>
+git branch <branch_name>
   
-    <span style="color: gray;"># Cambiar a una rama específica</span>
-    git checkout <branch_name>
+<span style="color: gray;"># Cambiar a una rama específica</span>
+git checkout <branch_name>
   
-    <span style="color: gray;"># Fusionar una rama específica en la rama actual</span>
-    git merge <branch_name>
+<span style="color: gray;"># Fusionar una rama específica en la rama actual</span>
+git merge <branch_name>
   
-    <span style="color: gray;"># Actualizar el repositorio local con los cambios desde el repositorio remoto</span>
-    git pull
+<span style="color: gray;"># Actualizar el repositorio local con los cambios desde el repositorio remoto</span>
+git pull
   
-    <span style="color: gray;"># Subir los cambios locales al repositorio remoto</span>
-    git push
+<span style="color: gray;"># Subir los cambios locales al repositorio remoto</span>
+git push
 
-    <span style="color: gray;"># Este comando realiza dos acciones en una sola línea</span>
-    <span style="color: gray;"># Agrega los archivos modificados y/o eliminados y crea un commit</span>
-    git commit -am "Mensaje del commit"
-  </pre>   
+<span style="color: gray;"># Este comando realiza dos acciones en una sola línea</span>
+<span style="color: gray;"># Agrega los archivos modificados y/o eliminados y crea un commit</span>
+git commit -am "Mensaje del commit"
+</pre>   
   `;
 }
 
