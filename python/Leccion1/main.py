73818bc7dc57425bc1cc958d887b45c35a0dd2e8
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-04-29 21:36:06 -0300

    update leccion1 python

---
+++ python/Leccion1/main.py
@@ -0,0 +1,14 @@
+miVariable = 3
+print(miVariable)
+miVariable = "Hola a todos los alumnos de la tecnicatura!"
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(f"El resultado es: {z}")
+# Las literales se escriben x832, y576, z896
+print(id(x))
+print(id(y))
+print(id(z))
