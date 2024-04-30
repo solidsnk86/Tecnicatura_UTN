dd55914cd276dab4f8f79ae436172776d9c882f9
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-04-30 10:59:58 -0300

    update python class

--- python/Leccion1/main.py
+++ python/Leccion1/main.py
@@ -1,14 +1,7 @@
-miVariable = 3
-print(miVariable)
-miVariable = "Hola a todos los alumnos de la tecnicatura!"
-print(miVariable)
-miVariable = 3.5
-print(miVariable)
-x = 10
-y = 2
-z = x + y
-print(f"El resultado es: {z}")
-# Las literales se escriben x832, y576, z896
-print(id(x))
-print(id(y))
-print(id(z))
+alto = int(input("Proporciona el alto del rectángulo: "))
+ancho = int(input("Proporciona el ancho del rectángulo: "))
+area = alto + ancho
+perimetro = (alto + ancho) * 2
+
+print("Área:", area)
+print("Perímetro:", perimetro)
