a6a01800eba8fc860ee49d5946038d4c2baa7bf5
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-06 19:49:22 -0300

    actualización clase 5 python

---
+++ python/leccion2/clase_5.py
@@ -0,0 +1,13 @@
+# La pregunta es si un padre puede asistir al juego de su hijo.​
+# Verificamos si tiene vacaciones​
+# Verificamos si tiene el día libre​
+# Usar estructura ‘if else’ con el operador or​
+# Imprimir
+
+vacaciones = True
+dia_libre = True
+
+if not (vacaciones or dia_libre):
+    print("El viejo tiene trabajo que hacer!")
+else:
+    print("El papá puede asistir al juego.")
