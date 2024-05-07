aa3b3da993ccd96eec45e46a24aa3787b18300fb
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-06 21:13:48 -0300

    actualización ejercicio 3

--- python/leccion2/ejercicio3.py
+++ python/leccion2/ejercicio3.py
@@ -9,11 +9,12 @@ edad_20 = 20 <= edad_usuario < 30
 edad_30 = 30 <= edad_usuario < 40
 
 # Imprimir resultados intermedios
-print("¿Estás entre 20 y 30 años (excluyendo 30)?", edad_20)
-print("¿Estás entre 30 y 40 años (excluyendo 40)?", edad_30)
+print("¿Estás entre 20 y 30 años?", edad_20)
+
+print("¿Estás entre 30 y 40 años?", edad_30)
 
 # Comprobar si la edad está en cualquier rango y dar salida final
 if edad_20 or edad_30:
-    print("Estás dentro del rango entre 20 y 40 años (sin incluir 40).")
+    print("Estás dentro del rango entre 20 a 30 años.")
 else:
-    print("No estás dentro del rango entre 20 y 40 años.")
+    print("No estás dentro del rango entre 20 a 30 años.")
