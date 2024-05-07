02b574fa33a293b6a4b44b9baf4e3babac01e650
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-06 21:16:32 -0300

    saltos de línea

--- python/leccion2/ejercicio3.py
+++ python/leccion2/ejercicio3.py
@@ -9,10 +9,11 @@ edad_20 = 20 <= edad_usuario < 30
 edad_30 = 30 <= edad_usuario < 40
 
 # Imprimir resultados intermedios
+print("")
 print("¿Estás entre 20 y 30 años?", edad_20)
 
 print("¿Estás entre 30 y 40 años?", edad_30)
-
+print("")
 # Comprobar si la edad está en cualquier rango y dar salida final
 if edad_20 or edad_30:
     print("Estás dentro del rango entre 20 a 30 años.")
