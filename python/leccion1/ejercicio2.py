2d54924c5dbabca368da35d94f35e88deaae98de
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-10 15:26:28 -0300

    Con esto agregas todos los archivos

---
+++ python/leccion1/ejercicio2.py
@@ -0,0 +1,5 @@
+# Ejercicio 2
+print("Se solicita incluir la siguiente información acerca de un libro:")
+libro = input("Proporciona el título: ")
+autor = input("Proporciona el autor: ")
+print("El libro es:",libro,"fue escrito por",autor)
