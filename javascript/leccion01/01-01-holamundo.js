a3e656a5886a2f7e9df723b572d5fc82ff655de0
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-11 16:23:23 -0300

    se agrego el archivo3

--- javascript/leccion01/01-01-holamundo.js
+++ javascript/leccion01/01-01-holamundo.js
@@ -1,4 +1,16 @@
+let myArray = ["h", "o", "l", "a", "m", "u", "n", "d", "o"];
 
+const mixArray = (num) => {
+  for (let i = num.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [num[i], num[j]] = [num[j], num[i]];
+  }
+  return num;
+};
 
+const mixedArray = mixArray([...myArray]);
 
-
+if (mixedArray) {
+  document.write(myArray.join(" ").toUpperCase());
+  document.writeln("<p>" + mixedArray.join(" ") + "</p>");
+}
--- python/leccion1/ejercicio2.py
+++ python/leccion1/ejercicio2.py
@@ -1,5 +1,7 @@
 # Ejercicio 2
 print("Se solicita incluir la siguiente información acerca de un libro:")
+print("")
 libro = input("Proporciona el título: ")
 autor = input("Proporciona el autor: ")
-print("El libro es:",libro,"fue escrito por",autor)
+print("")
+print(f"El libro es: {libro} y fue escrito por {autor}.")
