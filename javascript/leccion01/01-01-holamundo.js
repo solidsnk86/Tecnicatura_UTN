a956c90626c5f84f643807d17aff064ccee3d620
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-11 19:09:39 -0300

    update

--- javascript/leccion01/01-01-holamundo.js
+++ javascript/leccion01/01-01-holamundo.js
@@ -1,16 +1,18 @@
 let myArray = ["h", "o", "l", "a", "m", "u", "n", "d", "o"];
 
-const mixArray = (num) => {
+function mixArray(num) {
   for (let i = num.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [num[i], num[j]] = [num[j], num[i]];
   }
   return num;
-};
+}
 
 const mixedArray = mixArray([...myArray]);
 
 if (mixedArray) {
   document.write(myArray.join(" ").toUpperCase());
   document.writeln("<p>" + mixedArray.join(" ") + "</p>");
+
+  const miNimbre = () => {};
 }
--- python/leccion1/ejercicio1.py
+++ python/leccion1/ejercicio1.py
@@ -3,6 +3,6 @@ titulo = "Califica tu día:"
 print(titulo)
 pregunta = "¿Cómo estuvo tu día (1 al 10)? : "
 pregunta = int(input(pregunta))
-print("Mi día estuvo de:",pregunta)
+print("Mi día estuvo de:", pregunta)
 
 print("")
