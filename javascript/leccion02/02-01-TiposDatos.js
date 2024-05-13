d0ff97c3083fd9a963f4c84552ab21c8e6e278b1
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-12 21:59:10 -0300

    actualización ejercicios javascript

---
+++ javascript/leccion02/02-01-TiposDatos.js
@@ -0,0 +1,20 @@
+// Tipos de Datos en JavaScript
+
+/*
+La sintaxias en lo que es comentarios es
+muy similar a la de Java
+realmente diríamos que es idéntica
+*/
+var nombre = "Gabriel"; // Tipo Str
+console.log(nombre);
+
+var numero = 3000; // Tipo Number
+console.log(numero);
+
+var objeto = {
+  nombre: "Gabriel",
+  apellido: "Calcagni",
+  telefono: "02656480445",
+};
+
+console.log(objeto);
