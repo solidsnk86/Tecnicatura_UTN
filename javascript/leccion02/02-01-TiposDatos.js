55cf2e196dbbc5182f067433b9316f7b3372e69d
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-12 23:05:38 -0300

    actualizacion clase 5 javascript

--- javascript/leccion02/02-01-TiposDatos.js
+++ javascript/leccion02/02-01-TiposDatos.js
@@ -1,5 +1,4 @@
 // Tipos de Datos en JavaScript
-
 /*
 La sintaxias en lo que es comentarios es
 muy similar a la de Java
@@ -32,3 +31,32 @@ console.log(typeof miFuncion);
 // Tipo de dato Symbol
 var simbolo = Symbol("Mi símbolo");
 console.log(typeof simbolo);
+
+// Tipo de dato Clase
+class Persona {
+  constructor(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+  }
+}
+console.log(typeof Persona);
+
+// Tipo de dato undefined
+var x;
+console.log(typeof x);
+
+x = undefined;
+console.log(typeof x);
+
+// null: significa ausencia de un valor
+var y = null; // null no es un tipo de datos, pero su origen es de tipo object
+console.log(typeof y);
+
+// Tipo de dato array y Empty String
+var autos = ["Citroen", "Audi", "BMW", "Ford"];
+console.log(autos);
+console.log(typeof autos); // Preguntamos que tipo de datos es:
+
+var z = "";
+console.log(z); // Empty string (cadena vacía)
+console.log(typeof z);
