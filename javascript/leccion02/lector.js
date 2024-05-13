f004aeb7be355550d2b385a7c4d316fd961249f9
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-13 10:13:41 -0300

    actualización script fetch

--- javascript/leccion02/lector.js
+++ javascript/leccion02/lector.js
@@ -1,9 +1,8 @@
 const $body = document.body;
 const archivoTXT = "/javascript/Sintáxis JS y Java.txt";
-const archivoJS = "/javascript/leccion02/02-01-TiposDatos.js";
 
 $body.style.background = "#21252B";
-$body.style.color = "#fff"
+$body.style.color = "#fff";
 $body.style.width = "50%";
 $body.style.justifyContent = "center";
 $body.style.margin = "0 auto";
@@ -17,18 +16,6 @@ function crearHTML(datos) {
   `;
 }
 
-function crearPRE(codigo) {
-  $body.innerHTML = `
-  <div>
-    <pre>
-    <code lang="javascript">
-      ${codigo}
-    </code>
-    </pre>
-  </div>
-  `;
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const leerArchivo = async () => {
     const respuesta = await fetch(archivoTXT);
@@ -37,11 +24,5 @@ document.addEventListener("DOMContentLoaded", () => {
     return crearHTML(formatedTXT);
   };
 
-  const fetchJS = async () => {
-    const respuesta = await fetch(archivoJS);
-    const js = await respuesta.text();
-    return crearPRE(js);
-  };
   leerArchivo();
-  fetchJS();
 });
