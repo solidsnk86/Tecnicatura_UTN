b6f533b3b4d7885889bbb67ab20e3ceb3781633d
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-12 21:17:11 -0300

    update javascript basics exercises

--- javascript/leccion01/01-01-holamundo.js
+++ javascript/leccion01/01-01-holamundo.js
@@ -1,16 +1,4 @@
-let miArray = ["h", "o", "l", "a", "m", "u", "n", "d", "o"];
+var nombre = "Gabriel\n"
+var apellido = "Calcagni"
 
-function mixArray(num) {
-  for (let i = num.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [num[i], num[j]] = [num[j], num[i]];
-  }
-  return num;
-}
-
-const arrayMezclado = mixArray([...miArray]);
-console.log(mixArray);
-if (arrayMezclado) {
-  document.write(miArray.join(" ").toUpperCase());
-  document.writeln("<p>" + arrayMezclado.join(" ") + "</p>");
-}
+console.log(nombre, apellido)
