5514b89ff6e1839d6b3e4bfea5fc176944ed7d0d
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-13 01:51:29 -0300

    cambio de estilos

--- javascript/leccion02/lector.js
+++ javascript/leccion02/lector.js
@@ -1,7 +1,9 @@
 const $body = document.body;
 const archivoTXT = "/javascript/Sintáxis JS y Java.txt";
+const archivoJS = "/javascript/leccion02/02-01-TiposDatos.js";
+
 $body.style.background = "#21252B";
-$body.style.color = "#fff";
+$body.style.color = "#fff"
 $body.style.width = "50%";
 $body.style.justifyContent = "center";
 $body.style.margin = "0 auto";
@@ -15,6 +17,18 @@ function crearHTML(datos) {
   `;
 }
 
+function crearPRE(codigo) {
+  $body.innerHTML = `
+  <div>
+    <pre>
+    <code lang="javascript">
+      ${codigo}
+    </code>
+    </pre>
+  </div>
+  `;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const leerArchivo = async () => {
     const respuesta = await fetch(archivoTXT);
@@ -22,5 +36,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const formatedTXT = txt.replace(/\n/g, "<br>");
     return crearHTML(formatedTXT);
   };
+
+  const fetchJS = async () => {
+    const respuesta = await fetch(archivoJS);
+    const js = await respuesta.text();
+    return crearPRE(js);
+  };
   leerArchivo();
+  fetchJS();
 });
