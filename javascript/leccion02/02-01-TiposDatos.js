d90f67e2205efb9286ac1406ecdcaa3f02348e99
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-12 22:28:37 -0300

    actualización clase 4 javascript

--- javascript/leccion02/02-01-TiposDatos.js
+++ javascript/leccion02/02-01-TiposDatos.js
@@ -6,15 +6,29 @@ muy similar a la de Java
 realmente diríamos que es idéntica
 */
 var nombre = "Gabriel"; // Tipo Str
-console.log(nombre);
-
+console.log(typeof nombre);
+nombre = 7;
+console.log();
 var numero = 3000; // Tipo Number
-console.log(numero);
-
+console.log(typeof numero);
+nombre = 12.3;
+console.log(typeof nombre);
 var objeto = {
-  nombre: "Gabriel",
-  apellido: "Calcagni",
+  nombre: "Manuel",
+  apellido: "Calavera",
   telefono: "02656480445",
 };
 
-console.log(objeto);
+console.log(typeof objeto);
+
+// Tipo de dato Boolean
+var bandera = true;
+console.log(bandera);
+
+// Tipo de dato función
+function miFuncion() {}
+console.log(typeof miFuncion);
+
+// Tipo de dato Symbol
+var simbolo = Symbol("Mi símbolo");
+console.log(typeof simbolo);
