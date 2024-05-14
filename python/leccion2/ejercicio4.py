3947acf00c57c7072b3bc136f883c91c077a5117
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-14 14:31:44 -0300

    math

--- python/leccion2/ejercicio4.py
+++ python/leccion2/ejercicio4.py
@@ -1,12 +1,12 @@
 # Ejercicio 4: Área y longitud de un circulo​
 #Hacer un programa para ingresar el radio de un circulo y se reporte su área y 
 # la longitud de la circunferencia.​
-import math
+from math import pi
 
 radio = float(input("Ingrese el radio del círculo: "))
 
-area = math.pi * radio ** 2
-longitud = 2 * math.pi * radio
+area = pi * radio ** 2
+longitud = 2 * pi * radio
 
 print(f"El área del círculo es: {area}")
 print(f"La longitud de la circunferencia es: {longitud}")
