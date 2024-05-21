53ca915a0897b4ae9fa60c6bf08c2f835d221e72
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-21 17:28:57 -0300

    actualización ejercicos java clase 4 tipos de datos

--- java/leccion1/HolaMundo.java
+++ java/leccion1/HolaMundo.java
@@ -48,16 +48,49 @@ public class HolaMundo {
          * System.out.println("Comillas Dobles \"" + nombre + "\""); // Diagonal inversa
          * + comilla doble
          */
+        /*
+         * try (// Clase Scanner
+         * Scanner entrada = new Scanner(System.in)) {
+         * System.out.println("Digite su nombre: ");
+         * var usuario2 = entrada.nextLine();
+         * System.out.println("usuario2: " + usuario2);
+         * System.out.println("Escriba el título: ");
+         * var titulo2 = entrada.nextLine();
+         * System.out.println("Resultado: " + titulo2 + " " + usuario2);
+         * }
+         */
 
-        try (// Clase Scanner
-                Scanner entrada = new Scanner(System.in)) {
-            System.out.println("Digite su nombre: ");
-            var usuario2 = entrada.nextLine();
-            System.out.println("usuario2: " + usuario2);
-            System.out.println("Escriba el título: ");
-            var titulo2 = entrada.nextLine();
-            System.out.println("Resultado: " + titulo2 + " " + usuario2);
-        }
+        /*
+         * byte numEnteroByte = 127;
+         * System.out.println("numeroEnteroByte = " + numEnteroByte);
+         * System.out.println("Valor mínimo del byte: " + Byte.MIN_VALUE);
+         * System.out.println("Valor máximo de byte: " + Byte.MAX_VALUE);
+         * System.out.println("");
+         * short numeroEnteroShort = 32767;
+         * System.out.println("numeroEnteroShort = " + numeroEnteroShort);
+         * System.out.println("Valor mínimo del short: " + Short.MIN_VALUE);
+         * System.out.println("Valor máximo del short: " + Short.MAX_VALUE);
+         * 
+         * int numeroEnteroInt = 2147483647;
+         * System.out.println("numeroEnteroInt = " + numeroEnteroInt);
+         * System.out.println("Valor mínimo del int: " + Integer.MIN_VALUE);
+         * System.out.println("Valor máximo del int: " + Integer.MAX_VALUE);
+         * System.out.println("");
+         * long numeroEnteroLong = 9223372036854775807L;
+         * System.out.println("numeroEnteroLong = " + numeroEnteroLong);
+         * System.out.println("El valor mínimo de long: " + Long.MIN_VALUE);
+         * System.out.println("El valor máximo del long: " + Long.MAX_VALUE);
+         */
+        System.out.println("");
+        float numeroEnteroFloat = 3.4028235E38F;
+        System.out.println("numeroEnteroFloat = " + numeroEnteroFloat);
+        System.out.println("Valor mínimo del float: " + Float.MIN_VALUE);
+        System.out.println("Valor máximo del float: " + Float.MAX_VALUE);
+        System.out.println("");
+        double numeroEnteroDouble = 1.7976931348623157E308;
+        System.out.println("numeroEnteroDouble = " + numeroEnteroDouble);
+        System.out.println("Valor mínimo de double: " + Double.MIN_VALUE);
+        System.out.println("Valor máximo de double: " + Double.MAX_VALUE);
 
     }
 
