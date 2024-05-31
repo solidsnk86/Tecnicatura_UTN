f30da40940de2fa3462617c9701af136256bc089
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-31 00:41:22 -0300

    actualización concatenación de cadenas en javascript

--- javascript/leccion02/02-01-ConcatenacionDeCadenas.js
+++ javascript/leccion02/02-01-ConcatenacionDeCadenas.js
@@ -1,5 +1,5 @@
 var nombre = "Pedro";
-var apellido = "Montes";
+var apellido = " Montes";
 var nombreCompleto = nombre + " " + apellido; // Primera concatenación
 console.log(nombreCompleto);
 
@@ -7,7 +7,10 @@ var nombreCompleto2 = "Diego" + " " + "De la Vega"; // Segunda concatenación
 console.log(nombreCompleto2);
 var juntos = nombre + 219;
 console.log(juntos);
-juntos = nombre + 78 + 17; // Aquí se puede diferenciar a través de os paréntisis
+juntos = nombre + 78 + 17; // Aquí se puede diferenciar a través de los paréntisis
 console.log(juntos);
-juntos = 78 + 17 + nombre;
+juntos = 78 + 17 + nombre; // Primero se resuleve de izquierda a derecha (Va a sumar primero y después concatenar)
 console.log(juntos);
+
+nombre += apellido; // Operador simplificado
+console.log(nombre);
