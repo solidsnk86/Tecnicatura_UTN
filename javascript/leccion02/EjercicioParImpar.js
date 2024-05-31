7214c17c66a58240cd7324a547e9d2d03fe8b216
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-31 12:27:08 -0300

    Ejercicio para encontrar un número par o impar javascript clase 7

---
+++ javascript/leccion02/EjercicioParImpar.js
@@ -0,0 +1,12 @@
+var num1 = 6;
+var num2 = 3;
+
+function parImpar(num) {
+  if (num % 2 === 0) {
+    console.log(`El número ${num} es Par.`);
+  } else {
+    console.log(`El número ${num} es Impar.`);
+  }
+}
+
+parImpar(num2);
