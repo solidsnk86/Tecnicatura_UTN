2bfc8bd22230afb7d86306963e174e484c90ee05
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-31 00:11:02 -0300

    ciclo for in range mas palabra reservada continue clase 8 pyhton

--- python/leccion3/main.py
+++ python/leccion3/main.py
@@ -35,9 +35,21 @@ else:
 """
 
 # Palabra reservada break
+'''
 for letra in 'Alemania':
     if letra == 'a':
         print(f'Letra encontrada: {letra}')
         break
 else:
     print('Fin del ciclo for')
+'''
+
+# Palabra reservada continue
+for i in range(5):
+    if i % 2 == 0:
+        print(f"Valor {i}")
+print("Segundo Ejercicio:")
+for i in range(6):
+    if i % 2 != 0:
+        continue
+    print(f"Valor {i}")
