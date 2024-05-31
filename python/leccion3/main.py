4251ce338c48ac69022a7b8958905e37b434afeb
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-05-30 23:26:52 -0300

    actualización ejercicio con ciclo while clase 8

---
+++ python/leccion3/main.py
@@ -0,0 +1,17 @@
+# Ciclo While (Mientras - Durante)
+'''contador = 0
+while contador < 9:
+    print(f"Ta rrre loco vo wacho.. {contador}")
+    contador += 1
+else:
+    ("Fin del ciclo while")
+    
+'''
+# Imprimir número de 0 al 5 con el ciclo while
+maximo = 5
+contador = 0
+while contador <= maximo:
+    print(contador)
+    contador += 1
+    
+    
