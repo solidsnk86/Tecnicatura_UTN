5229f4058a1e68602a69276fc294f32572e52497
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-06-04 17:05:03 -0300

    se agregaron ejercicios 1 y 2 de python clase 9

--- python/ejercicios_clase_9/ejercicio_1.py
+++ python/ejercicios_clase_9/ejercicio_1.py
@@ -1,19 +1,18 @@
 # Diseñar un programa que ingresado un año, mos devuelva por
-# consola si es bisiesto o no.
+# consola si es bisiesto o no.'''
 print("Programa para determinar si el año es bisiesto.")
 
 
-def leap_year(num):
+def ano_bisiesto(num):
     if (num % 4 == 0 and num % 100 != 0) or (num % 400 == 0):
         print(f"El año {num} es bisiesto.")
     else:
         print(f"El año {num} no es bisiesto.")
 
-
 opcion = 1
 while opcion == 1:
     num = int(input("Ingrese un año: "))
-    leap_year(num)
+    ano_bisiesto(num)
     opcion = int(
         input(
             "Para seguir adelante digite la opción 1, para salir digite cualquier otro número: "
