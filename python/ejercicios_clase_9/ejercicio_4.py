b806308107078a061e5ddf9e5907bbcf3cec41c5
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-06-04 21:06:17 -0300

    se cambió método de interpolación en un solo print

--- python/ejercicios_clase_9/ejercicio_4.py
+++ python/ejercicios_clase_9/ejercicio_4.py
@@ -1,7 +1,9 @@
 # Ejercicio 4: Suponga que se tiene un conjunto de calificaciones de un grupo de
 # 10 alumnos. Realizar un algoritmo para calcular la calificación promedio y
 # y la calificacipon más baja de todo el grupo.
-
+print(
+    "Programa para calcular la calificación promedio y la calificación más baja de todo el grupo."
+)
 suma = 0
 calificacion_baja = 99999
 
@@ -12,5 +14,10 @@ for i in range(10):
         calificacion_baja = calificacion
 
 calificacion_promedio = suma / 10
-print(f"La calificaión promedio es: {calificacion_promedio}")
-print(f"La calificación más baja es {calificacion_baja}")
+print("")
+print(
+    f"""
+    La calificación promedio es: {calificacion_promedio}
+    La calificación más baja es: {calificacion_baja}
+    """
+)
