db842b092f49c51b538bd4e5832c92f500a68925
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-06-21 23:17:49 -0300

    actualización ejercicio clase 11 java

---
+++ java/CondicionalEjercicio_4/src/main/java/com/neo/condicionalejercio_4/CondicionalEjercicio_4.java
@@ -0,0 +1,28 @@
+// Determinar si un alumno aprueba o reprueba un curso, sabiendo que
+// aprobará si su promedio de calificaciones es mayor o igual a 70
+// o reprueba caso contrario.
+
+package com.neo.condicionalejercio_4;
+import java.util.Scanner;
+
+public class CondicionalEjercicio_4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        var calificaciones = 3;
+        var suma =0;
+        
+        for (var i = 0; i < calificaciones; i++) {
+            System.out.println("Digite la " + (i + 1) + "° calificacion: ");
+            var nota = input.nextInt();
+            suma += nota;
+        }
+        
+        var promedio = suma / 3;
+        
+        if (promedio >= 70) {
+            System.out.println("La calificacion del alumno es: " + promedio + ". Esta Aprobado");
+        } else {
+            System.out.println("La calificacion del alumno es: " + promedio + ". Esta Desaprobado");
+        }
+    }
+}
