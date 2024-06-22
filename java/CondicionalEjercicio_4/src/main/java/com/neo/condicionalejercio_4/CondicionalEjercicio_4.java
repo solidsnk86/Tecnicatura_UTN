b8dc7c6293a4c1569d7be1bb6bbc9673fef95e6a
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-06-22 20:00:18 -0300

    Corección ejercicio

--- java/CondicionalEjercicio_4/src/main/java/com/neo/condicionalejercio_4/CondicionalEjercicio_4.java
+++ java/CondicionalEjercicio_4/src/main/java/com/neo/condicionalejercio_4/CondicionalEjercicio_4.java
@@ -2,28 +2,29 @@
 // aprobará si su promedio de calificaciones es mayor o igual a 70
 // o reprueba caso contrario.
 
-package com.neo.condicionalejercio_4;   
-import java.util.Scanner;
+package com.neo.condicionalejercio_4;
+
 import java.text.MessageFormat;
+import java.util.Scanner;
 
 public class CondicionalEjercicio_4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         var calificaciones = 3;
-        var suma =0;
-        
+        int suma = 0;
+        System.out.println("Programa para determinar la calificación del usuario: (Rango aceptado en %, ej: 70)");
         for (var i = 0; i < calificaciones; i++) {
-            System.out.println("Digite la " + (i + 1) + "° calificacion: ");
-            var nota = input.nextInt();
+            System.out.println("Digite la " + (i + 1) + "° calificación: ");
+            double nota = Double.parseDouble(input.nextLine());
             suma += nota;
         }
-        
+
         var promedio = suma / 3;
-        
+
         if (promedio >= 70) {
-            System.out.println(MessageFormat.format("La calificacion del alumno es: {0}. Esta Aprobado", promedio));
+            System.out.println(MessageFormat.format("La calificación del alumno es: {0}%. Esta Aprobado", promedio));
         } else {
-            System.out.println("La calificacion del alumno es: " + promedio + ". Esta Desaprobado");
+            System.out.println(MessageFormat.format("La calificación del alumnno es {0}%. Está desaprobado", promedio));
         }
     }
 }
