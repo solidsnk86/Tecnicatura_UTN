6cf46f2db776c7c7bb3c6fc9ca13eb5afa22de1d
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-08-15 14:03:28 -0300

    ejercicios python

--- python/leccion4/ejercicio_1.py
+++ python/leccion4/ejercicio_1.py
@@ -1,7 +1,8 @@
+"""
+Ejercicio 1: iterar un rango de 0 a 10 e imprimir números divisibles entre 3
+Ejemplo de ejecución: 0,3,6,9
+"""
+
 for i in range(10):
     if i % 3 == 0:
         print(i)
-print("[[[[[[]]]]]]")
-for j in range(2, 7):
-    j = +j
-    print(j)
