4c86ad6ce8f1889005f0e8eb2fd5f43286bc10ee
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-08-15 12:11:35 -0300

    Clases en java con break, continue y labels

--- java/Leccion2/CicloWhile/src/main/java/com/neo/ciclowhile/CicloWhile.java
+++ java/Leccion2/CicloWhile/src/main/java/com/neo/ciclowhile/CicloWhile.java
@@ -14,9 +14,23 @@ public class CicloWhile {
             System.out.println("Contador = " + contador);
             contador++;
         } while(contador <= 7);
-       
+        
+        // Uso de la sintaxis break, continue y etiquetas (labels)
+        
         for (var count = 0; count < 7; count++) {
+            if (count % 2 == 0) {
+                System.out.println("El conteo es: " + count);
+                break ;
+            }
+        }
+        inicio:
+        for (var count = 0; count < 7; count++) {
+            if (count % 2 != 0) {
+                continue inicio; // Vamos a la siguiente iteración
+            }
             System.out.println("El conteo es: " + count);
         }
+        
+        
     }
 }
