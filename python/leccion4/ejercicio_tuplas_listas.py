ac16208ca4a4b696bf4c156e33f87afc9d3c6899
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-08-15 16:22:10 -0300

    actualizacion ejercicio

--- python/leccion4/ejercicio_tuplas_listas.py
+++ python/leccion4/ejercicio_tuplas_listas.py
@@ -1,4 +1,7 @@
+# Dada la siguiente tupla
 tupla = (13, 1, 8, 3, 2, 5, 8)
+# Creamos una listaque solo incluya los números menores a 5
+# e imprima por consola [1, 2, 3]
 
 lista = []
 for num in tupla:
