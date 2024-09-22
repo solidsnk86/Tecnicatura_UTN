d9e6dbf4ce3b46dae924dcc38a11aa45cb1756e9
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-09-22 18:20:06 -0300

    Se agregó clase 6.3 java

--- java/Leccion4/Aritmetica/src/main/java/Operaciones/Aritmetica.java
+++ java/Leccion4/Aritmetica/src/main/java/Operaciones/Aritmetica.java
@@ -5,8 +5,18 @@ import javax.swing.JOptionPane;
 public class Aritmetica {
     // Atributos de la clase
     int a, b;
-
-    // Métodos
+    
+    // El constructor es un método especial
+    public Aritmetica() { // Constructor 1 vacío
+        System.out.println("Se está ejecutnado el constructor N°1");
+    }
+    // Estamos viendo lo que se llama sobrecarga de contructores
+    public Aritmetica(int a, int b) {
+        this.a = a;
+        this.b = b;
+        System.out.println("Se está ejecutando el constructor N°2");
+    }
+    // Método
     public void sumarNumeros() {
         int resultado = a + b;
         JOptionPane.showMessageDialog(null, resultado);
--- java/Leccion4/Aritmetica/src/main/java/Operaciones/PruebaAritmetica.java
+++ java/Leccion4/Aritmetica/src/main/java/Operaciones/PruebaAritmetica.java
@@ -1,16 +1,34 @@
 package Operaciones;
 
 public class PruebaAritmetica {
+
     public static void main(String[] args) {
+        int a = 10; // Variables locales
+        int b = 7; // Memoria stack
+        miMetodo(); // Se ejecuta el nuevo amétodo aquí
         Aritmetica aritmetica1 = new Aritmetica();
         aritmetica1.a = 3;
         aritmetica1.b = 7;
         // aritmetica1.sumarNumeros();
         
+        // Para almacenar un objeto o los atributos se utiliza la meoria heap
         int resultado = aritmetica1.sumarConRetorno();
-        System.out.println("El resultado es: "+resultado);
-        
+        System.out.println("El resultado es: " + resultado);
+
         resultado = aritmetica1.sumarConArgumentos(8, 9);
-        System.out.println("Resultado con argumentos: "+resultado);
+        System.out.println("Resultado con argumentos: " + resultado);
+
+        System.out.println("aritmetia1 a: " + aritmetica1.a);
+        System.out.println("aritmetia1 b: " + aritmetica1.b);
+        
+        Aritmetica aritmetica2 = new Aritmetica(5,8);
+        System.out.println("aritmetica2 a ="+ aritmetica2.a);
+        System.out.println("aritmetica2 b ="+ aritmetica2.b);
+        // System.gc(); Garbage Colector - Método para limpiar residuos NO UTILIZAR
+    }
+    
+    public static void miMetodo() {
+        //int a = 10; // Una variable está limitada
+        System.out.println("Aqui hay otro método");
     }
 }
