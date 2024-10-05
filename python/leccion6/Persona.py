78cf888af2c355bf40c3a52f1d2d37ce6bd3cb39
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-10-05 15:54:03 -0300

    actualización clase 7 python completa [Encapsulamiento en Python]

--- python/leccion6/Persona.py
+++ python/leccion6/Persona.py
@@ -1,17 +1,22 @@
 class Persona:  # Creamos una clase
     # pass  # No se procesa nada más (No tiene contenido)
     def __init__(
-        self, nombre, apellido, edad
+        self, nombre, apellido, dni, edad, *args, **kwargs
     ):  # Se lo llama método Init Dunder (Double underscore)
-        self.nombre = nombre
+        self.__nombre = nombre
         self.apellido = apellido
+        self._dni = dni  # Este atributo está encapsulado de una manera segura
         self.edad = edad
+        self.args = args
+        self.kwargs = kwargs
 
     def mostrar_detalle(self):
-        print(f"Persona: {self.nombre} {self.apellido}, edad: {self.edad}")
+        print(
+            f"La clase Persona tiene los siguientes datos: {self.__nombre} {self.apellido}: {self._dni}, edad: {self.edad}, la dirección es: {self.args}, los datos importantes son {self.kwargs}"
+        )
 
 
-persona1 = Persona("Manuel", "Calavera", 369)
+persona1 = Persona("Manuel", "Calavera", 2369874, 369)
 print(
     f"El objeto1 de la clase Persona: {persona1.nombre} {persona1.apellido}, {persona1.edad} años"
 )
@@ -21,7 +26,7 @@ print(persona1.apellido)
 print(persona1.edad, "años")
 
 
-persona2 = Persona("Goro", "MK", 1000)
+persona2 = Persona("Goro", "MK", 1323579, 1000)
 print(
     f"El objeto2 de la clase persona: {persona2.nombre} {persona2.apellido}, {persona2.edad} años"
 )
@@ -41,6 +46,29 @@ persona1.mostrar_detalle()  # La referencia se pasa de manera automática
 persona1.telefono = "2654989966"  # Hemos creado un atributo de un objeto
 print(f"Este es el teléfono {persona1.telefono} de {persona1.nombre}")
 
-print(
-    persona2.telefono
-)  # Esto nos dará error, el objeto persona2 no tiene este atributo
+# print(
+#     persona2.telefono
+# )  # Esto nos dará error, el objeto persona2 no tiene este atributo
+
+persona3 = Persona(
+    "Solid",
+    "Snake",
+    32099014,
+    42,
+    "Teléfono",
+    "26656480141",
+    "Calle Konami",
+    551,
+    "Manzana",
+    97,
+    "Casa",
+    38,
+    Altura=1.71,
+    Peso=72,
+    Color_Favorito="Verde",
+    Auto="Honda",
+    Modelo=2007,
+)
+persona3.mostrar_detalle()
+print(persona3._dni)  # En VScode se ve OJO 👀 (Supuestamente está encapsulado 😲)
+# persona3.__nombre # Está totalmente encapsulado
