d4a5330f6b6c051c6a1215c4007cabb89802c8f4
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-10-05 20:35:22 -0300

    Método toString() en la clase Java [8.3]

--- java/Leccion5/Encapsulamiento/src/main/java/test/PersonaPrueba.java
+++ java/Leccion5/Encapsulamiento/src/main/java/test/PersonaPrueba.java
@@ -13,12 +13,20 @@ public class PersonaPrueba {
         // persona1.nombre = "Román" // Esto ya no se puede utilizar porque es de acceso privado
         // System.out.println(persona1.nombre); Error
         System.out.println("persona1 con su nombre modificado = " + persona1.getNombre());
-        System.out.println("El sueldo de persona1 es: $"+ persona1.getSueldo());
-        System.out.println("persona1 se ha eliminado?: "+ persona1.isEliminado());
-        
+        System.out.println("El sueldo de persona1 es: $" + persona1.getSueldo());
+        System.out.println("persona1 se ha eliminado?: " + persona1.isEliminado());
+
         Persona persona2 = new Persona("Mario", 369000, true);
-        System.out.println("persona2 = "+ persona2.getNombre());
-        System.out.println("El sueldo de persona2 es: $"+persona2.getSueldo());
-        System.out.println("persona2 se ha eliminado?: "+ persona2.isEliminado());
+        System.out.println("persona2 = " + persona2.getNombre());
+        System.out.println("El sueldo de persona2 es: $" + persona2.getSueldo());
+        System.out.println("persona2 se ha eliminado?: " + persona2.isEliminado());
+
+        persona2.setNombre("Gerardo");
+        persona2.setEliminado(true);
+        persona2.setSueldo(936000);
+        System.out.println("persona2 modificada: " + persona2.getNombre() + ". Su sueldo es: $" + persona2.getSueldo() + ". Usuario existente?: " + persona2.isEliminado());
+
+        System.out.println("persona2 = " + persona2);
+
     }
 }
