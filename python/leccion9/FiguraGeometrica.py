7ba2da251e6886e3b21bb2c4ad8e3d7df2e0a012
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-10-21 18:23:24 -0300

    clase 9 Herencia Múltiple POO [Clase Cuadrado]

---
+++ python/leccion9/FiguraGeometrica.py
@@ -0,0 +1,6 @@
+class FiguraGeometrica:
+    def __init__(self, alto, ancho):
+        self._alto = alto
+        self._ancho = ancho
+        
+    
---
+++ python/leccion9/test_figura_geometrica.py
@@ -0,0 +1,7 @@
+from Cuadrado import Cuadrado
+
+cuadrado1 = Cuadrado(5, "azul")
+print(cuadrado1._alto)
+print(cuadrado1._ancho)
+print(cuadrado1._color)
+print(f"Cálculo del área {cuadrado1.calcular_area()}")
