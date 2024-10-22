e167be27e839e0caa6651c21e58404ed245caa85
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-10-22 00:06:32 -0300

    clase 10 python validaciones setter

--- python/leccion9/FiguraGeometrica.py
+++ python/leccion9/FiguraGeometrica.py
@@ -1,23 +1,40 @@
 class FiguraGeometrica:
     def __init__(self, ancho, alto):
-        self._alto = alto
-        self._ancho = ancho
-
-    @property
-    def alto(self):
-        return self._alto
-
-    @alto.setter
-    def set_alto(self, alto):
-        self._alto = alto
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            self._ancho = 0
+            print(f"El valor es erróneo para el ancho {ancho}.")
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            self._alto = 0
+            print(f"El valor es erróneo para el alto {alto}.")
 
     @property
     def ancho(self):
         return self._ancho
 
     @ancho.setter
-    def set_ancho(self, ancho):
-        self._ancho = ancho
+    def ancho(self, ancho):
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            print(f"Valor erróneo ancho {ancho}.")
+
+    @property
+    def alto(self):
+        return self._alto
+
+    @alto.setter
+    def alto(self, alto):
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            print(f"Valor erróneo alto {alto}.")
 
     def __str__(self) -> str:
         return f"Figura Geométrica: [Ancho {self._ancho}, Alto: {self._alto}]"
+
+    def _validar_valores(self, valor):  # Método encapsulado
+        return True if 0 < valor < 10 else False
