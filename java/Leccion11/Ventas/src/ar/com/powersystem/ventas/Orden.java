6893abe658db846f4207aec6e4969b0b1f0673db
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-10-31 00:40:27 -0300

    Clase 12 diseños de clases JAVA

---
+++ java/Leccion11/Ventas/src/ar/com/powersystem/ventas/Orden.java
@@ -0,0 +1,34 @@
+package ar.com.powersystem.ventas;
+
+public class Orden {
+
+    private int idOrden;
+    private Producto productos[];
+    public static int contadorOrdenes;
+    private int contadorProductos;
+    static final int MAX_PRODUCTOS = 10;
+
+    public Orden() {
+        this.idOrden = ++Orden.contadorOrdenes;
+        this.productos = new Producto[Orden.MAX_PRODUCTOS];
+    }
+
+    public void agregarProducto(Producto producto) {
+        if (this.contadorProductos < Orden.MAX_PRODUCTOS) {
+            this.productos[this.contadorProductos++] = producto;
+        } else {
+            System.out.println("Se ha superado el máximo de productos: " + Orden.MAX_PRODUCTOS);
+        }
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (int i = 0; i < this.contadorProductos; i++) {
+//            Producto producto = this.productos[i];
+//            total += producto.getPrecio();
+            total += this.productos[i].getPrecio();
+        }
+        return total;
+    }
+
+}
